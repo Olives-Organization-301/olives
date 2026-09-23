ab7f9094ecd976b7a74caa366f8a072b8323684c
Adriel <abanaag@ualberta.ca>
2026-09-22 18:18:01 -0600

    commit Color Additions

--- olives/app/src/main/java/com/example/olives/Triangle.kt
+++ olives/app/src/main/java/com/example/olives/Triangle.kt
@@ -3,10 +3,11 @@ package com.example.olives
 class Triangle (
     x: Int,
     y: Int,
+    color: String,
     sideType: String = "equilateral",
     angleType: String = "acute",
     sideLengthA: Int,
     sideLengthB: Int,
     sideLengthC: Int,
-) : Shape(x,y) {
+) : Shape(x,y,color) {
 }
